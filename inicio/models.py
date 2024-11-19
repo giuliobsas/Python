d13fcdea25f5cf9dc0a4cfa02b503ff5a312324a
Giulio Brongo <businessbsas@gmail.com>
2024-11-18 21:17:18 -0300

    Se actualizaron las tareas a cumplir

--- inicio/models.py
+++ inicio/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class cliente(models.Model): 
+    
+    nombre = models.CharField(max_length=20)
+    marca = models.CharField(max_length=20)
+    color = models.CharField(max_length=20)
+    
